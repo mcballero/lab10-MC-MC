63f43e474ea5c0461c7a6eb58699209421154641
mariacaballero <caballerom@ufl.edu>
2025-04-10 13:35:07 -0400

    unit tests maria

--- test_calculator.py
+++ test_calculator.py
@@ -2,54 +2,26 @@ import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
-    ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    # Partner 1
+    def test_multiply(self): # 3 assertions
+        self.assertEqual(mul(123, 76), 9348)
+        self.assertEqual(mul('95', '89'), "can't multiply sequence by non-int of type 'str'")
+        self.assertEqual(mul(-5, -97), 485)
 
-    ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_log_invalid_argument(self): # 1 assertion
+        self.assertEqual(logarithm(0, 0), "Error: Neither input can be 0")
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
 
-    ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_hypotenuse(self): # 3 assertions
+        self.assertAlmostEqual(hypotenuse(7.5, 2.5), 7.905)
+        self.assertEqual(hypotenuse(3, 4), 5.0)
+        self.assertEqual(hypotenuse('NUM', 12), "Error: must be real number, not str")
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
-
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
-    
-    ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
-
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
-
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+    def test_sqrt(self): # 3 assertions
+        self.assertEqual(square_root(0), 0.0)
+        self.assertEqual(square_root(4), 2)
+        self.assertAlmostEqual(square_root(2), 1.4142)
 
 # Do not touch this
 if __name__ == "__main__":
